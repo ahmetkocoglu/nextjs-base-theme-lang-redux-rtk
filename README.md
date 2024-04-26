6ef4b0383bd86e6e7d29dd1873f884bbdc496a1a
akocoglu <ahmetkocoglu@outlook.com>
2024-04-26 12:01:07 +0300

    lang and theme

--- README.md
+++ README.md
@@ -1,34 +1,3 @@
-# README #
-
-This README would normally document whatever steps are necessary to get your application up and running.
-
-### What is this repository for? ###
-
-* Quick summary
-* Version
-* [Learn Markdown](https://bitbucket.org/tutorials/markdowndemo)
-
-### How do I get set up? ###
-
-* Summary of set up
-* Configuration
-* Dependencies
-* Database configuration
-* How to run tests
-* Deployment instructions
-
-### Contribution guidelines ###
-
-* Writing tests
-* Code review
-* Other guidelines
-
-### Who do I talk to? ###
-
-* Repo owner or admin
-* Other community or team contact
-  This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
-
 ## Getting Started
 
 First, run the development server:
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,8 @@
 import {useLocale, useTranslations} from 'next-intl';
 import LocalSwitcher from "@/components/local-switcher";
 import React from "react";
-import Link from "next/link";
+import {ThemeProvider} from "@/components/theme-provider"
+import {ModeToggle} from "@/components/mode";
 
 export default function Home() {
   const t = useTranslations('IndexPage');
@@ -11,9 +12,19 @@ export default function Home() {
     <div className="text-center">
       <h1 className='text-4xl mb-4 font-semibold'>{t('title')}</h1>
       <p>{t('description')}</p>
-        <LocalSwitcher/>
-        <br/>
-        <Link href={`/${localActive}/editor`}>Editor</Link>
+        <div className="flex justify-center">
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+            <div className="fixed flex flex-1">
+                <LocalSwitcher/>
+                <ModeToggle/>
+            </div>
+        </ThemeProvider>
+        </div>
     </div>
   );
 }
