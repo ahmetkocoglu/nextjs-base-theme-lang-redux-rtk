5551f3871bf858cc70d3bc98ed0987544507b38b
akocoglu <ahmetkocoglu@outlook.com>
2024-04-26 11:48:30 +0300

    init

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,14 @@
-'use client';
+import {useLocale, useTranslations} from 'next-intl';
+import React from "react";
 
 export default function NotFound() {
-  return (
-    <html>
-      <body className='text-center'>
-        <h1 className='mt-10 font-semibold'>Something went wrong!</h1>
-      </body>
-    </html>
-  );
+    const t = useTranslations('Error');
+
+    return (
+        <html>
+            <body className='text-center'>
+                <h1 className='mt-10 font-semibold'>{t('not-found')}</h1>
+            </body>
+        </html>
+    );
 }
--- src/layouts/Layouts.tsx
+++
@@ -1,5 +0,0 @@
-import NoLayout from "./NoLayout";
-export const Layouts = {
-    No: NoLayout,
-};
-export type LayoutKeys = keyof typeof Layouts; // "Auth" | "Base"
